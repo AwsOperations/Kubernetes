8a0d2b4c32a6ee61ba52f15d9df84404a56e5a51
AwsOperations <134834806+AwsOperations@users.noreply.github.com>
2023-06-15 13:04:36 +0530

    Add files via upload

---
+++ jenkins script/jenkins-server-script-ubuntu-servers.sh
@@ -0,0 +1,91 @@
+#!/bin/bash
+
+sudo apt update && sudo apt upgrade
+sudo apt install openjdk-11-jre
+java -version
+openjdk version "11.0.12" 2021-07-20
+OpenJDK Runtime Environment (build 11.0.12+7-post-Debian-2)
+OpenJDK 64-Bit Server VM (build 11.0.12+7-post-Debian-2, mixed mode, sharing)
+
+sudo apt update && sudo apt upgrade
+sudo mkdir -p /usr/share/keyrings
+curl -fsSL https://pkg.jenkins.io/debian-stable/jenkins.io.key | sudo tee /usr/share/keyrings/jenkins-keyring.asc > /dev/null
+echo deb [signed-by=/usr/share/keyrings/jenkins-keyring.asc] \
+https://pkg.jenkins.io/debian-stable binary/ | sudo tee /etc/apt/sources.list.d/jenkins.list > /dev/null
+sudo apt update
+sudo apt install jenkins
+systemctl status jenkins --no-pager -l
+sudo systemctl enable --now jenkins
+sudo cat /var/lib/jenkins/secrets/initialAdminPassword
+
+# wait for jenkins start up
+response=""
+key=""
+while [ `echo $response | grep 'Authenticated' | wc -l` = 0 ]; do
+  key=`sudo cat /var/lib/jenkins/secrets/initialAdminPassword`
+  echo $key >> /tmp/status.txt
+  response=`sudo java -jar /var/cache/jenkins/war/WEB-INF/lib/cli-2.401.1.jar -s http://localhost:8080 who-am-i --username admin --password $key`
+  sudo echo $response
+  sudo echo "Jenkins not started, wait for 2s"
+  sleep 2
+done >> /tmp/status.txt
+echo "Jenkins started" >> /tmp/status.txt
+echo "Install Plugins" >> /tmp/status.txt
+
+# sudo apt update
+# sudo apt install openjdk-11-jre
+# java -version
+# openjdk version "11.0.12" 2021-07-20
+# OpenJDK Runtime Environment (build 11.0.12+7-post-Debian-2)
+# OpenJDK 64-Bit Server VM (build 11.0.12+7-post-Debian-2, mixed mode, sharing)
+# curl -fsSL https://pkg.jenkins.io/debian-stable/jenkins.io-2023.key | sudo tee \
+#     /usr/share/keyrings/jenkins-keyring.asc > /dev/null
+# echo deb [signed-by=/usr/share/keyrings/jenkins-keyring.asc] \
+#     https://pkg.jenkins.io/debian-stable binary/ | sudo tee \
+#     /etc/apt/sources.list.d/jenkins.list > /dev/null
+# sudo apt update
+# sudo apt-get install jenkins -y
+# sudo systemctl enable jenkins
+# sudo systemctl start jenkins
+# sudo more /var/lib/jenkins/secrets/initialAdminPassword
+
+# # install jenkins
+
+# sudo apt update
+# sudo apt install openjdk-11-jre
+# java -version
+# openjdk version "11.0.12" 2021-07-20
+# OpenJDK Runtime Environment (build 11.0.12+7-post-Debian-2)
+# OpenJDK 64-Bit Server VM (build 11.0.12+7-post-Debian-2, mixed mode, sharing)
+# curl -fsSL https://pkg.jenkins.io/debian-stable/jenkins.io-2023.key | sudo tee \
+#   /usr/share/keyrings/jenkins-keyring.asc > /dev/null
+# echo deb [signed-by=/usr/share/keyrings/jenkins-keyring.asc] \
+#   https://pkg.jenkins.io/debian-stable binary/ | sudo tee \
+#   /etc/apt/sources.list.d/jenkins.list > /dev/null
+# sudo apt-get update
+# sudo apt-get install jenkins
+# sudo apt-get update
+# sudo apt-get install jenkins
+# # sudo yum upgrade -y
+# # sudo amazon-linux-extras install java-openjdk11 -y
+# sudo apt-get install jenkins -y
+# sudo systemctl enable jenkins
+# sudo systemctl start jenkins
+# sudo more /var/lib/jenkins/secrets/initialAdminPassword
+
+# # install git
+# sudo apt-get install git -y
+
+# install terraform
+
+# sudo yum install -y yum-utils
+# sudo yum-config-manager --add-repo https://rpm.releases.hashicorp.com/AmazonLinux/hashicorp.repo
+# sudo yum -y install terraform
+
+# install kubectl
+
+# sudo curl -LO https://storage.googleapis.com/kubernetes-release/release/v1.23.6/bin/linux/amd64/kubectl
+# sudo chmod +x ./kubectl
+# sudo mkdir -p $HOME/bin && sudo cp ./kubectl $HOME/bin/kubectl && export PATH=$PATH:$HOME/bin
+
+
