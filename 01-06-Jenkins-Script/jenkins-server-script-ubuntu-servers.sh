60cb048e446f8f19ebb0916aff8dda3c6d37c3c1
AwsOperations <134834806+AwsOperations@users.noreply.github.com>
2023-07-17 17:59:44 +0530

    Update jenkins-server-script-ubuntu-servers.sh

--- 01-06-Jenkins-Script/jenkins-server-script-ubuntu-servers.sh
+++ 01-06-Jenkins-Script/jenkins-server-script-ubuntu-servers.sh
@@ -1,21 +1,23 @@
-#!/bin/bash
-
-sudo apt update && sudo apt upgrade
-sudo apt install openjdk-11-jre
-java -version
-openjdk version "11.0.12" 2021-07-20
-OpenJDK Runtime Environment (build 11.0.12+7-post-Debian-2)
-OpenJDK 64-Bit Server VM (build 11.0.12+7-post-Debian-2, mixed mode, sharing)
-
-sudo apt update && sudo apt upgrade
-sudo mkdir -p /usr/share/keyrings
-curl -fsSL https://pkg.jenkins.io/debian-stable/jenkins.io.key | sudo tee /usr/share/keyrings/jenkins-keyring.asc > /dev/null
-echo deb [signed-by=/usr/share/keyrings/jenkins-keyring.asc] \
-https://pkg.jenkins.io/debian-stable binary/ | sudo tee /etc/apt/sources.list.d/jenkins.list > /dev/null
 sudo apt update
-sudo apt install jenkins
-systemctl status jenkins --no-pager -l
+
+sudo apt install -y openjdk-17-jre
+
+java -version
+ 
+curl -fsSL https://pkg.jenkins.io/debian-stable/jenkins.io-2023.key | sudo tee \
+  /usr/share/keyrings/jenkins-keyring.asc > /dev/null
+echo deb [signed-by=/usr/share/keyrings/jenkins-keyring.asc] \
+  https://pkg.jenkins.io/debian-stable binary/ | sudo tee \
+  /etc/apt/sources.list.d/jenkins.list > /dev/null
+
+sudo apt-get update
+
+sudo apt-get install jenkins
+
 sudo systemctl enable --now jenkins
+
+sudo systemctl start jenkins.service
+
 sudo cat /var/lib/jenkins/secrets/initialAdminPassword
 
 # # wait for jenkins start up
